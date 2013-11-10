038bd1853a4d5015d5c520c0773620fe44e9aac8
Drain <p960388@gmail.com>
2013-11-10 14:07:38 +0800

    add local storage function with JSON

--- main.lua
+++ main.lua
@@ -1,9 +1,7 @@
 require("Class")
 
 
-
-
+JSONFileSet = require "kalacool.sango.JSON.JSONFileSet"
 local storyboard = require "storyboard"
 scene = storyboard.newScene()
 storyboard.gotoScene( "kalacool.sango.Scene.Menu", "fade", 400 )
-
