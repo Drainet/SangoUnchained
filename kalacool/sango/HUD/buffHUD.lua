347710dbb60ee7781d3aa154af3347aa9651adb4
Pope99 <cyclops8099@gmail.com>
2013-12-22 18:06:56 +0800

    gun buff bug fix

--- kalacool/sango/HUD/buffHUD.lua
+++ kalacool/sango/HUD/buffHUD.lua
@@ -5,29 +5,42 @@ module(..., package.seeall)
 
 
 function new(time , image)
-
+	local buffHUD = {}
+	local buffImage = display.newGroup()
 	local self = display.newImage(image)
 	local myText = display.newText(time, 40, 200 + dog.buffNum*80 , native.systemFont, 60)
-	myText:setTextColor(0, 255, 0 )
-	
+	myText:setFillColor(0, 255, 0 )
+	buffHUD.image = buffImage
 	self.x=60
 	self.y=230+ dog.buffNum*80
 	HUD:insert(self)
 	HUD:insert(myText)
-
+	buffImage:insert(self)
+	buffImage:insert(myText)
+	
 	dog.buffNum = dog.buffNum+1
+	
 
-	function timeout(event)
+	function buffHUD.timeout(event)
 		myText.text = time-event.count
+		dog.image.shootFasterBuffTime = dog.image.shootFasterBuffTime -1
 		print(  time-event.count )
 		if(event.count==time)then
 			display.remove(self)
 			display.remove(myText)
 			self=nil
 			dog.buffNum = dog.buffNum-1
-		end
-		
+		end		
 	end
-	timer.performWithDelay( 1000, timeout , time )
+	buffHUD.timer = timer.performWithDelay( 1000, buffHUD.timeout , time )
 
+
+	function buffHUD.dispose()
+		timer.cancel( buffHUD.timer )
+		display.remove(buffHUD.image)
+		buffHUD.image = nil
+		dog.buffNum = dog.buffNum-1		
+	end
+	return buffHUD
 end
+
