a083a736541c81a4e7d22246130a1fc7a73104de
Capillary J <jason9075@gmail.com>
2014-02-23 17:46:35 +0800

    add power door

--- kalacool/sango/Classes/Objects/World/InteractiveWorld/Drum.lua
+++ kalacool/sango/Classes/Objects/World/InteractiveWorld/Drum.lua
@@ -12,7 +12,7 @@ function new(config)
     local Drum = InteractiveWorldClass.new()
     Drum.setImage(_World.InteractiveWorld.Drum.ImagePath)
     Drum.image.damage = _World.InteractiveWorld.Drum.Damage
-    --Drum.image.type = "explosive"
+    -- Drum.image.type = "explosive"
     Drum.show(config)
     Drum.dead = false
     physics.addBody( Drum.image, _World.InteractiveWorld.Drum.Physic.Type, _World.InteractiveWorld.Drum.Physic.Option)
@@ -22,16 +22,14 @@ function new(config)
     end
 
     function Drum:collision(event)
-
         if ((event.other.type == "bullet" or event.other.type == "explosive") and Drum.dead == false and event.phase == "began") then
             Drum.image:boom()
         end
 	end
 
-
     function Drum.image:boom()
         if(Drum.image.x~=nil) then
-        Animation:newExplode(Drum.image.x,Drum.image.y)
+            Animation:newExplode(Drum.image.x,Drum.image.y)
             local function createCircleSenser() 
                 if(Drum.image~=nil) then
                     local circle = display.newCircle( Drum.image.x, Drum.image.y, 300 )  
@@ -45,36 +43,32 @@ function new(config)
                     end
 
                     local function onLocalCollision(self, event )  
-                        if ( event.phase == "began" and self.myName == "circle" and event.other.type~= "bullet" and event.other.type ~= "explosive") then  
-                            xVelocity = event.other.x-self.x  
-                            yVelocity = event.other.y-self.y
-                            distance = math.sqrt( xVelocity*xVelocity + yVelocity*yVelocity)
-                            xVelocity = (xVelocity)/distance
-                            yVelocity = (yVelocity)/distance
+                            if ( event.phase == "began" and self.myName == "circle" and event.other.type~= "bullet" and event.other.type ~= "explosive") then  
+                                xVelocity = event.other.x-self.x  
+                                yVelocity = event.other.y-self.y
+                                distance = math.sqrt( xVelocity*xVelocity + yVelocity*yVelocity)
+                                xVelocity = (xVelocity)/distance
+                                yVelocity = (yVelocity)/distance
 
-                            if distance <=500 then
-                                distance = 500 - distance
-                                local ratio = distance / 500
-                                event.other:setLinearVelocity(ratio* xVelocity*3000, ratio*yVelocity *3000)
+                                if distance <=500 then
+                                    distance = 500 - distance
+                                    local ratio = distance / 500
+                                    event.other:setLinearVelocity(ratio* xVelocity*3000, ratio*yVelocity *3000)
+                                end
+
+                                if circle.removed ==  false then
+                                    timer.performWithDelay( 10, circle.removeSensor ,1 )
+                                    circle.removed =true
+                                end  
                             end
-
-                            if circle.removed ==  false then
-                                timer.performWithDelay( 10, circle.removeSensor ,1 )
-                                circle.removed =true
-                            end  
-                            
-                        
-                        end
                     end 
-                        circle.myName = "circle" 
-                        circle:setFillColor(0,0,0,0)
-                        physics.addBody( circle, "static" ,{ isSensor = true,radius =500,filter = { categoryBits = 32,maskBits = 39}}) 
-                        circle.collision = onLocalCollision  
-                        circle:addEventListener( "collision", circle )
-                        camera:insert(circle)    
-                        timer.performWithDelay( 10, circle.removeSensor ,1 )
-
-                        --physics.setDrawMode( "hybrid" )
+                    circle.myName = "circle" 
+                    circle:setFillColor(0,0,0,0)
+                    physics.addBody( circle, "dynamic" ,{ isSensor = true,radius =300,filter = { categoryBits = 32,maskBits = 39}}) 
+                    circle.collision = onLocalCollision  
+                    circle:addEventListener( "collision", circle )
+                    camera:insert(circle)    
+                    timer.performWithDelay( 10, circle.removeSensor ,1 )
                 end
             end
             timer.performWithDelay( 9,createCircleSenser,1) 
@@ -82,7 +76,6 @@ function new(config)
             Drum.dead = true
         end
     end
-
  	
  	Drum.image:addEventListener( "collision", Drum)
  	Drum.listeners[1] = {event='removeAllEvent' , listener = Drum}
--- kalacool/sango/Set/MechanismSet.lua
+++ kalacool/sango/Set/MechanismSet.lua
@@ -2,6 +2,9 @@ MechanismSet = {}
 	button =  require("kalacool.sango.Classes.Objects.Mechanisms.Triggers.Trigger_Button")
 	receiver =  require("kalacool.sango.Classes.Objects.Mechanisms.Receivers.Receiver_Rock")
 	disappearFloor = require("kalacool.sango.Classes.Objects.Mechanisms.SelfTriggers.SelfTrigger_DisappearFloor")
+	powerGenerator = require("kalacool.sango.Classes.Objects.Mechanisms.Triggers.Trigger_PowerGenerator")
+	powerDoor =  require("kalacool.sango.Classes.Objects.Mechanisms.Receivers.Receiver_PowerDoor")
+
 
 	function MechanismSet.newTriggerButton(config)
 	    local mechanism = button.new(config)
@@ -15,5 +18,15 @@ MechanismSet = {}
 
 	function MechanismSet.newDisappearFloor(config)
 		local mechanism  = disappearFloor.new(config)
+		return mechanism 
+	end
+
+	function MechanismSet.newPowerGenerator(config)
+		local mechanism  = powerGenerator.new(config)
+		return mechanism 
+	end
+
+	function MechanismSet.newPowerDoor (config)
+		local mechanism  = powerDoor.new(config)
 		return mechanism 
 	end
