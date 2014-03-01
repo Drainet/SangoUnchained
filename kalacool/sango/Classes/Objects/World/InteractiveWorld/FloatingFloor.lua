b5eba5279f34e20809041be325086e8a58ce40b7
Capillary J <jason9075@gmail.com>
2014-03-01 13:53:43 +0800

    unfinished

--- kalacool/sango/Classes/Objects/World/InteractiveWorld/FloatingFloor.lua
+++ kalacool/sango/Classes/Objects/World/InteractiveWorld/FloatingFloor.lua
@@ -14,20 +14,50 @@ function new(config)
     FloatingFloor.image.height = _World.InteractiveWorld.FloatingFloor.Height
     FloatingFloor.image.damage = _World.InteractiveWorld.FloatingFloor.Damage
     FloatingFloor.show(config)
+
+-------- Argument
+    -- floorOriginX        = config.x
+    -- floorOriginY        = config.y
+
+    floorDirection      = config.floorDirection     -- left, right, up, down
+    -- floorWattingTime    = config.floorWattingTime   -- ms
+    -- floorMovment        = config.floorMovment       -- pixel
+    -- floorDirection      = right
+    -- floorWattingTime    = 3000
+    -- floorMovment        = 200
+--------
+    FloatingFloor.count = 0
+    maxCount = 200
     physics.addBody( FloatingFloor.image,  _World.InteractiveWorld.FloatingFloor.Physic.Type, _World.InteractiveWorld.FloatingFloor.Physic.Option )
-    FloatingFloor.count=0
 
-    local maxCount = 100
+    local move = 0
     function FloatingFloor:floatingFloorMoving(event)
-        if( FloatingFloor.count == maxCount) then
-            FloatingFloor.count = 0 
-        end
-
-        FloatingFloor.count = FloatingFloor.count + 1   
-        if FloatingFloor.count == 1 then
-            FloatingFloor.image:setLinearVelocity(200,0)
-        elseif FloatingFloor.count == maxCount/2 + 1 then
-            FloatingFloor.image:setLinearVelocity(-200,0)
+        
+        -- if(math.round(FloatingFloor.image.x) == floorOriginX )  then
+        --     local function listener( event )
+        --         FloatingFloor.image:setLinearVelocity(50,0)
+        --     end
+        --     FloatingFloor.image.x = FloatingFloor.image.x + 1
+        --     FloatingFloor.image:setLinearVelocity(0,0)
+        --     timer.performWithDelay( floorWattingTime, listener )
+        -- elseif (math.round(FloatingFloor.image.x) == floorOriginX + floorMovment ) then
+        --     local function listener( event )
+        --         FloatingFloor.image:setLinearVelocity(-50,0)
+        --     end
+        --     FloatingFloor.image.x = FloatingFloor.image.x - 1
+        --     FloatingFloor.image:setLinearVelocity(0,0)
+        --     timer.performWithDelay( floorWattingTime, listener )
+        -- end
+        if(floorDirection == "right")then
+            if( FloatingFloor.count == maxCount) then
+                FloatingFloor.count = 0 
+            end
+            FloatingFloor.count = FloatingFloor.count + 1   
+            if FloatingFloor.count == 1 then
+                FloatingFloor.image:setLinearVelocity(200,0)
+            elseif FloatingFloor.count == maxCount/2 + 1 then
+                FloatingFloor.image:setLinearVelocity(-200,0)
+            end
         end
     end
 
@@ -36,20 +66,10 @@ function new(config)
     end
 
     function FloatingFloor.image:preCollision(event)
-
         local vx , vy = event.other:getLinearVelocity()
-        
-        -- if(vy<0)then
-            
-            -- event.contact.isEnabled=false
-        -- end
-
         if((event.otherElement~=1 or vy<0) and event.contact~=nil)then
-            
             event.contact.isEnabled=false
         end
-
-       
     end
     
     scene:addEventListener( 'floatingFloorMoving', FloatingFloor )
